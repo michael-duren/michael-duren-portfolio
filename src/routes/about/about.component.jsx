9ff42387b7500df29495979db9cfad22455cd5e8
Michael Duren <michaeld@michaelduren.com>
2022-10-11 19:22:56 -0500

    ADDED functionality of about menu, and rendering

--- src/routes/about/about.component.jsx
+++ src/routes/about/about.component.jsx
@@ -1,38 +1,77 @@
-import { AboutContainer, MichaelImage } from './about.component.styles';
+import { useState } from 'react';
+
+import {
+  AboutContainer,
+  NavContainer,
+  MenuButton,
+  MenuButtons,
+  CloseWindowX,
+  IconButton,
+  MichaelImage,
+} from './about.component.styles';
 
 import michaelAvatar from '../../assets/Avatar.png';
 
+const AboutUses = () => {
+  return <h1>THIS IS WHAT I USE</h1>;
+};
+
+const AboutOverview = () => {
+  return (
+    <div className="content-container">
+      <div className="icon-image">
+        <MichaelImage src={michaelAvatar} alt="Michael Duren" />
+      </div>
+      <div className="content">
+        <h1>
+          <b>michael</b> Duren
+        </h1>
+        <p>Michael Duren is a front end developer and musician based</p>
+        <p>out of St. Paul MN.</p>
+        <p>He specializes in React.js and Sass. </p>
+        <p>Currently Michael is seeking front end roles.</p>
+        <p>Michaels music has been featured on Spotify and Apple</p>
+        <p>music. Click the link in the dock to listen.</p>
+        <br />
+        <p>Currently Michael is working on learning Tailwind and React</p>
+        <p>Native. </p>
+      </div>
+    </div>
+  );
+};
+
 const About = () => {
+  const [content, setContent] = useState(true);
+
+  const onClickOverview = () => {
+    if (content) {
+      return;
+    } else {
+      setContent(!content);
+    }
+  };
+
+  const onClickUses = () => {
+    if (!content) {
+      return;
+    } else {
+      setContent(!content);
+    }
+  };
+
   return (
     <AboutContainer>
-      <div className="nav-container">
-        <div className="menu-buttons">
-          <button>Overview</button>
-          <button>Uses</button>
-        </div>
-        <div className="home-button">
-          <button className="x-button">X</button>
-        </div>
-      </div>
-      <div className="content-container">
-        <div className="icon-image">
-          <MichaelImage src={michaelAvatar} alt="Michael Duren" />
-        </div>
-        <div className="content">
-          <h1>
-            <b>michael</b> Duren
-          </h1>
-          <p>Michael Duren is a front end developer and musician based</p>
-          <p>out of St. Paul MN.</p>
-          <p>He specializes in React.js and Sass. </p>
-          <p>Currently Michael is seeking front end roles.</p>
-          <p>Michaels music has been featured on Spotify and Apple</p>
-          <p>music. Click the link in the dock to listen.</p>
-          <br />
-          <p>Currently Michael is working on learning Tailwind and React</p>
-          <p>Native. </p>
-        </div>
-      </div>
+      <NavContainer>
+        <MenuButtons>
+          <MenuButton onClick={onClickOverview}>Overview</MenuButton>
+          <MenuButton onClick={onClickUses}>Uses</MenuButton>
+          <IconButton to="/">
+            <CloseWindowX />
+          </IconButton>
+        </MenuButtons>
+      </NavContainer>
+      {content && <AboutOverview />}
+      {!content && <AboutUses />}
     </AboutContainer>
   );
 };
--- src/routes/about/about.component.styles.jsx
+++ src/routes/about/about.component.styles.jsx
@@ -1,8 +1,52 @@
 import styled from 'styled-components';
 
+import { ReactComponent as CloseWindow } from '../../assets/close-window.svg';
+
+import { Link } from 'react-router-dom';
+
 export const AboutContainer = styled.div`
-  display: flex;
+  /* display: flex; */
   flex-direction: column;
+  flex-grow: 1;
+  height: 50vh;
+  min-height: 20rem;
+  align-items: center;
+  background-color: rgba(244, 244, 244, 0.5);
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 60vw;
+  border-radius: 2rem;
+`;
+
+export const NavContainer = styled.div`
+  margin-top: 2rem;
+`;
+
+export const MenuButtons = styled.div`
+  display: flex;
+  /* align-items: flex-start; */
+  justify-content: space-evenly;
+`;
+
+export const MenuButton = styled.button`
+  background-color: rgba(244, 244, 244, 0.3);
+  border-radius: 2rem;
+  font-size: 1.5rem;
+  cursor: pointer;
+`;
+
+export const IconButton = styled(Link)`
+  border-radius: 2rem;
+  background-color: rgba(244, 244, 244, 0.3);
+  cursor: pointer;
+
+  /* position: fixed; */
+`;
+
+export const CloseWindowX = styled(CloseWindow)`
+  height: 1.5rem;
 `;
 
 export const MichaelImage = styled.img`
