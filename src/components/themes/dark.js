9fb2173ed29038e494b7ab23994a991baf19189e
Michael Duren <michaeld@michaelduren.com>
2022-10-23 13:46:36 -0500

    Added light/dark themes final

---
+++ src/components/themes/dark.js
@@ -0,0 +1,10 @@
+const theme = {
+  id: 'dark',
+  primaryColor: '#393E46',
+  secondaryColor: '#00ADB5',
+  dropShadowColor: '#00FFF5',
+  bodyBackgroundColor: '#222831',
+  bodyFontColor: 'white',
+};
+
+export default theme;
---
+++ src/components/themes/light.js
@@ -0,0 +1,10 @@
+const theme = {
+  id: 'light',
+  primaryColor: '#DED5C4',
+  secondaryColor: '#305973',
+  dropShadowColor: '#EF7E56',
+  bodyBackgroundColor: '#F9F9F9',
+  bodyFontColor: '305973',
+};
+
+export default theme;
